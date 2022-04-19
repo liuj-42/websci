b87b49e42e510fe4af95eb52b8c8037617e44b2d
James Liu <65922029+liuj-42@users.noreply.github.com>
2022-04-19 11:47:52 -0400

    quiz 2 commit

--- quiz/src/app/app.component.ts
+++ quiz/src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quiz';
+  shown = 0;
+  child1Shown = false;
+  child2Shown = false;
+
+  child1Event(data:string) {
+    console.log(data)
+    if (!this.child1Shown) {
+      this.shown += 1;
+      this.child1Shown = true;
+    }
+  }
+
+  child2Event(data:string) {
+    if (!this.child2Shown){
+      this.shown += 1;
+      this.child2Shown = true;
+    }
+
+  }
 }
--- quiz/src/app/app.module.ts
+++ quiz/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ChildGraph1Component } from './child-graph1/child-graph1.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ChildGraph2Component } from './child-graph2/child-graph2.component';
+import { BarComponent } from './bar/bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildGraph1Component,
+    ChildGraph2Component,
+    BarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
